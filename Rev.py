0ae0e7bc2f4e8b95b595a847fe66ee220eac4f30
Madhavi <madhu18061991@gmail.com>
2023-10-20 13:37:41 +0530

    addding code from line no 9 to 15

--- Rev.py
+++ Rev.py
@@ -5,3 +5,11 @@ str = input()
 strRev = str[::-1]
 str = strRev
 print("\nReverse =", str)
+
+#i am adding this lines
+print("Enter the String: ", end="")# end use for add string
+str = input()
+
+strRev = str[::-1]
+str = strRev
+print("\nReverse =", str)
